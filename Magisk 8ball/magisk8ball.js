67666a3576a3d4c140ea01368c512d432ae18aa2
Krabbe7 <143596266+Krabbe7@users.noreply.github.com>
2023-10-15 16:02:55 +0200

    Add files via upload

---
+++ Magisk 8ball/magisk8ball.js
@@ -0,0 +1,78 @@
+// sætte variabler til dom-elementer
+const Knap2 = document.getElementById("knap2");
+const Container2 = document.getElementById("container2");
+const Container1 = document.getElementById("container1");
+const Container3 = document.getElementById("container3");
+const SvarOmrade = document.getElementById("svarOmrade");
+const ImageElement = document.getElementById("eightball");
+const Overskrift = document.getElementById("overskrift");
+const FaaSvar = document.getElementById("knap2");
+const NytSprg = document.getElementById("knap3");
+const Input = document.getElementById("sporgsmaal");
+
+// Startfunktion, til at kunne stille spørgsmål
+const start = () => {
+  ImageElement.setAttribute('src', 'eightball_grafik_sporgsmaal.png');
+  Container2.style.display = "flex";
+  Container1.style.display = "none";
+  Container3.style.display = "none";
+  SvarOmrade.style.display = "none";
+};
+
+// Funktion til at eightball giver svar baseret på en array der udvælger svar tilfældigt
+const visSvar = () => {
+  
+  const eightBall = {
+    advice: ["Ja", "Nej", "Måske", "Ingen </br> chance", "Tjooo, </br> hvorfor ikke", "Prøv igen </br> senere"],
+    index: 0,
+    answer: function() {
+      this.index = Math.floor(Math.random() * this.advice.length);
+    },
+    look: function() {
+      return this.advice[this.index];
+    }
+  };
+
+  // Vælg et tilfældigt svar fra 'eightBall'
+  eightBall.answer();
+  const randomAnswer = eightBall.look();
+
+  // Opdater DOM-elementer og layout ved svar
+  SvarOmrade.innerHTML = randomAnswer;
+  SvarOmrade.style.display = "block";
+  ImageElement.setAttribute('src', 'eightball_grafik_svar.png');
+  Overskrift.style.display = "none";
+  FaaSvar.style.display = "none";
+  NytSprg.style.display = "block";
+  Input.style.display = "none";
+  Container3.style.display = "flex";
+};
+
+// Funktion til at nulstille og starte et nyt spørgsmål
+const nytSprg = () => {
+  ImageElement.setAttribute('src', 'eightball_grafik_sporgsmaal.png');
+  Overskrift.style.display = "block";
+  FaaSvar.style.display = "block";
+  NytSprg.style.display = "none";
+  Input.style.display = "block";
+  SvarOmrade.style.display = "none";
+
+  // Nulstil inputfeltet
+  Input.value = "";
+};
+
+// Funktion for at starte ved at trykke på enter-tasten
+function init() {
+  let måned = document.getElementById("sporgsmaal");
+  måned.onkeydown = handleKeyPress;
+}
+
+// Funktion til at håndtere Enter-tasten og få svar-knap
+const handleKeyPress = (e) => {
+  if (e.keyCode === 13) {
+    Knap2.click();
+    return false;
+  }
+};
+// Funktion til at starte ved indlæsning af siden
+window.onload = init();
---
+++ Stopur/stopur.js
@@ -0,0 +1,72 @@
+// Vent med at køre koden, indtil siden er fuldt indlæst
+window.onload = function () {
+    // Initialiser variabler til at holde tidspunktet
+    let minutter = 0;
+    let sekunder = 0;
+    let milisek = 0;
+
+    // Hent referencer til HTML-elementer
+    let tilfojMilisek = document.getElementById("milisekDisplay");
+    let tilfojSekunder = document.getElementById("sekunderDisplay");
+    let tilfojMinutter = document.getElementById("minutterDisplay");
+
+    // Hent referencer til start, stop og reset knapper
+    let buttonStart = document.getElementById("button-start");
+    let buttonStop = document.getElementById("button-stop");
+    let buttonReset = document.getElementById("button-reset");
+
+    let interval; // Variabel til at gemme interval-ID for setInterval
+
+    // klik på start-knappen
+    buttonStart.onclick = function () {
+        clearInterval(interval); // Stop tidligere tidtagning, hvis det er aktivt
+        interval = setInterval(startTimer, 10); // Start et nyt interval hvert 10. millisekund
+    };
+
+    // klik på stop-knappen
+    buttonStop.onclick = function () {
+        clearInterval(interval); // Stop det aktuelle tidtagning
+    };
+
+    // klik på reset-knappen
+    buttonReset.onclick = function () {
+        clearInterval(interval); // Stop det aktuelle tidtagning
+        // Nulstil tidspunkt og opdater HTML-elementer
+        milisek = 0;
+        sekunder = 0;
+        minutter = 0;
+        tilfojMilisek.innerHTML = "00";
+        tilfojSekunder.innerHTML = "00";
+        tilfojMinutter.innerHTML = "00";
+    };
+
+    // Funktion til at starte tidtagning
+    function startTimer() {
+        milisek++;
+
+        // Opdater HTML-elementet for milisekunder
+        if (milisek <= 9) {
+            tilfojMilisek.innerHTML = "0" + milisek;
+        }
+
+        if (milisek > 9) {
+            tilfojMilisek.innerHTML = milisek;
+        }
+
+        // Håndter overgang til sekunder
+        if (milisek > 99) {
+            sekunder++;
+            tilfojSekunder.innerHTML = sekunder <= 9 ? "0" + sekunder : sekunder;
+            milisek = 0;
+            tilfojMilisek.innerHTML = "00";
+        }
+
+        // Håndter overgang til minutter
+        if (sekunder > 59) {
+            minutter++;
+            tilfojMinutter.innerHTML = minutter <= 9 ? "0" + minutter : minutter;
+            sekunder = 0;
+            tilfojSekunder.innerHTML = "00";
+        }
+    }
+};
